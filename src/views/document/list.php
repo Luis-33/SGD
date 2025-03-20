f4ec5af65f927876f15dea3331c299841cad076e
Alan Eduardo Vazquez Mora <lalomora250396@gmail.com>
2025-03-19 20:51:30 -0600

    add delete document modal to document list view

--- src/views/document/list.php
+++ src/views/document/list.php
@@ -60,7 +60,7 @@ include_once UTIL_PATH . 'ModalEditDocumento.php';
             <div class="table_body" id="tableContainer">
 
                 <?php foreach ($documents as $document) : ?>
-                <?php   echo generateModalEditDocument($document["documento_id"]); ?>
+                    <?php   echo generateModalEditDocument($document["documento_id"]); ?>
                     <div class="table_body_item">
                         <span class="row_pdf" title="Descargar <?php echo $document['documento_tipo']; ?>">
                             <a href="download.php?docID=<?php echo $document['documento_id']; ?>"><i class="fa-solid fa-file-pdf"></i></a>
@@ -102,6 +102,7 @@ include_once UTIL_PATH . 'ModalEditDocumento.php';
                         <?php endif; ?>
                     </div>
                 <?php endforeach; ?>
+                <?php echo generateModal('deleteDocument', 'Eliminar documento', true); ?>
             </div>
             <div class="no_result_message" id="noResultsMessage" style="display: none;">
                 <span>No se encontraron coincidencias.</span>
